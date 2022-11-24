d4effbf51e789b7d5bf22fcb2b1b3104e200b02b
Nefise Ercik <nefiseercik.ne@gmail.com>
2022-11-24 22:51:00 +0300

    nefiseV6

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,3 +1,15 @@
 public class Runner {
 
+
+
+    public static void main(String[] args) {
+        Ogretmen ogretmen=new Ogretmen();
+        ogretmen.setIsim("nefise");
+        ogretmen.setSoyIsim("ercik");
+        ogretmen.setTcNo("26598745213");
+        ogretmen.setDogumTarihi(1984);
+        System.out.println(ogretmen);
+        System.out.println(ogretmen.getIsim());
+    }
+
 }
