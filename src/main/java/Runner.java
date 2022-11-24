e790b5ef36405051f81c7e5028e5fe324acf7232
huseyinercik <huseyincanercik@gmail.com>
2022-11-25 00:44:01 +0200

    huseyinV8

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -10,6 +10,16 @@ public class Runner {
         ogretmen.setDogumTarihi(1984);
         System.out.println(ogretmen);
         System.out.println(ogretmen.getIsim());
+
+        Ogrenci ogrenci=new Ogrenci();
+        ogrenci.setIsim("huseyin");
+        ogrenci.setSoyIsim("ercik");
+        ogrenci.setTcNo("56587412369");
+        ogrenci.setDogumTarihi(1983);
+        ogrenci.setOkulNo("1325");
+        System.out.println(ogrenci);
+
+
     }
 
 }
